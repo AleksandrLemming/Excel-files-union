3b3e5e804f5c12d2f83c46a78886b831efc42d93
AleksandrBaiduzh <bayduzh11@gmail.com>
2017-05-11 19:55:04 +0300

    Excel files union

---
+++ Excel files union for MMW app/Excel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+
+namespace Excel_files_union_for_MMW_app
+{
+    class Excel
+    {
+        Application excelapp = new Application();
+
+        private string pathfolder = null;
+        public string[] filelists = null;
+        public int rowsinheader = 1;
+        public int sheetnumber = 1;
+
+        public void PathFolder(string s)
+        {
+            string datetime = DateTime.Now.Hour.ToString() + "_" + DateTime.Now.Minute.ToString()
+                    + "_" + DateTime.Now.Second.ToString(); // добавляем уникальности - время создания
+
+            pathfolder = s + "\\UnionFile_Creation Time " + datetime + ".xlsb";
+        }
+
+        public void UnionFiles() //Объединяем файлы
+        {
+            #region Создаем эксель приложение и новую книгу в которой будут все данные из других книг
+                
+                Workbook wb = excelapp.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+                Worksheet ws = wb.Worksheets[1];
+
+                ws.Name = "UnionData";
+                    wb.SaveAs(pathfolder, XlFileFormat.xlExcel12, Type.Missing, Type.Missing, Type.Missing,
+                    Type.Missing, XlSaveAsAccessMode.xlNoChange, Type.Missing, Type.Missing,
+                    Type.Missing, Type.Missing, Type.Missing);
+            #endregion
+
+            //открываем по очереди наши эксель файлы
+            for (int i = 0; i < filelists.Length; i++)
+            {
+                Workbook wbFrom = excelapp.Workbooks.Open(filelists[i]);
+                Worksheet wsFrom = wbFrom.Worksheets[sheetnumber];
+                //копируем диапазон
+                if (i == 0) //если книга первая, то надо скопировать с шапкой таблицы, если нет - копируем лишь содержимое
+                {
+                    Range areaFrom = wsFrom.Range[wsFrom.Cells[1, 1], wsFrom.Cells.SpecialCells(XlCellType.xlCellTypeLastCell)];
+                    Range areaTo = ws.Cells[1, 1];
+                    areaFrom.Copy(areaTo);
+                }
+                else
+                {
+                    int rowcount = ws.Cells.SpecialCells(XlCellType.xlCellTypeLastCell).Row; //надо определить строку, чтобы не копировать на одно и тоже место
+                    Range areaFrom = wsFrom.Range[wsFrom.Cells[rowsinheader + 1, 1], wsFrom.Cells.SpecialCells(XlCellType.xlCellTypeLastCell)];
+                    Range areaTo = ws.Cells[rowcount + 1, 1];
+                    areaFrom.Copy(areaTo);
+                }
+                wbFrom.Close(false);
+            }
+
+            wb.Close(true);
+            excelapp.Quit();
+        }
+    }
+}
---
+++ Excel files union for MMW app/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Excel_files_union_for_MMW_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Excel excel = null;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.excel = new Excel();
+            listBox1.HorizontalScrollbar = true;
+            listBox1.ScrollAlwaysVisible = true;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); //очищаем листбокс
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) //если папка выбрана, то выводим список файлов в листбокс и загоняем их в массив
+            {
+                excel.PathFolder(folderBrowserDialog1.SelectedPath); //запоминаем путь к папке
+
+                excel.filelists = Directory.GetFiles(folderBrowserDialog1.SelectedPath); //список загоняем в массив
+
+                foreach (var item in excel.filelists) //заполняем листбокс списком файлов из выбранной папки
+                {
+                    listBox1.Items.Add(item);
+                }
+            }
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0) //если папка пуста или не выбрана, то выдаст сообщение
+            {
+                WindowState = FormWindowState.Minimized;
+
+                excel.UnionFiles();
+
+                WindowState = FormWindowState.Normal;
+                MessageBox.Show("Все готово!");
+                listBox1.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Вы не выбрали папку или в папке нету файлов!");
+            }
+        }
+
+        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            excel.rowsinheader = Convert.ToInt32(numericUpDown1.Value);
+        }
+
+        private void NumericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            excel.sheetnumber = Convert.ToInt32(numericUpDown1.Value);
+        }
+    }
+}
